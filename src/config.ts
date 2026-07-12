ad58d9d6d4cf38fe74cf30e8c5a532696d9ad184
percent-zero <percentzero@lpc>
2026-07-12 03:28:04 -0400

    chore(config): add config.ts to repo

---
+++ src/config.ts
@@ -0,0 +1,34 @@
+const config = {
+	prefix: '$',
+
+	fonts: {
+		basic: {
+			fontName: 'IthkuilBasic',
+			path: './resources/fonts/IthkuilBasic.ttf',
+			name: 'Ithkuil Basic',
+		},
+		flow: {
+			fontName: 'IthkuilFlow',
+			path: './resources/fonts/IthkuilFlow.ttf',
+			name: 'Ithkuil Flow',
+		},
+	},
+
+	rendering: {spaceBetweenWords: false, // spaces between words feature is in beta
+	},
+
+	// Quizzes
+	quizzes: {
+		numberTries: 5,
+		inversionByDefault: true,
+		timeoutMs: 1000*20,
+		questionTimoutMs: 1000*7,
+		maxQuestionTimeoutMs: 1000*15,
+		// how long to wait before giving an answer and giving the next question
+		answerQuestionIntervalMs: 1000*2.5,
+		maxUninteractedQuestions: 3,
+		maxLength: 20,
+	},
+};
+
+export default config;
